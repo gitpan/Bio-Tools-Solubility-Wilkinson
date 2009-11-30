1dbd68440d68fb9102409d3536b41924dc01ca3d
Bruno Vecchi <brunov@cpan.org>
2009-11-30 21:02:58 +0000

    Import of BRUNOV/Bio-Tools-Solubility-Wilkinson-0.093340 from CPAN.
    gitpan-cpan-distribution: Bio-Tools-Solubility-Wilkinson
    gitpan-cpan-version:      0.093340
    gitpan-cpan-path:         BRUNOV/Bio-Tools-Solubility-Wilkinson-0.093340.tar.gz
    gitpan-cpan-author:       BRUNOV
    gitpan-cpan-maturity:     released

---
+++ lib/Bio/Tools/Solubility/Wilkinson.pm
@@ -0,0 +1,149 @@
+package Bio::Tools::Solubility::Wilkinson;
+our $VERSION = '0.093340';
+
+
+
+# ABSTRACT: Calculate the probability of a protein to be soluble using the Wilkinson-Harrison model
+
+use strict;
+use warnings;
+use Sub::Exporter -setup => {
+    exports => ['solubility'],
+    groups  => { default => ['solubility'] },
+};
+
+use constant {
+    L1  =>  15.43,
+    L2  => -29.56,
+    CVp =>   1.71,
+    A   =>   0.4934,
+    B   =>   0.276,
+    C   =>  -0.0392,
+};
+
+
+sub solubility {
+    my $protein = shift or die "No protein argument";
+
+    my $CV      = _CV($protein);
+    my $CV_norm = abs($CV - CVp);
+
+    my $probability = A + B * $CV_norm + C * ($CV_norm**2);
+
+    return $probability;
+}
+
+sub _CV {
+    my $protein = shift;
+
+    my %n = map { $_ => _aa_count($protein, $_) } qw(N G P S R K D E);
+    my $n = length $protein;
+
+    my $cv =
+        L1 * (   ($n{N} + $n{G} + $n{P} + $n{S}) / $n       )
+      + L2 * abs(($n{R} + $n{K} - $n{D} - $n{E}) / $n - 0.03);
+
+    return $cv;
+
+}
+
+sub _aa_count {
+    my ($protein, $aa) = @_;
+
+    my @occurences = $protein =~ /$aa/ig;
+
+    return scalar @occurences;
+
+}
+
+1;
+
+
+
+=pod
+
+=head1 NAME
+
+Bio::Tools::Solubility::Wilkinson - Calculate the probability of a protein to be soluble using the Wilkinson-Harrison model
+
+=head1 VERSION
+
+version 0.093340
+
+=head1 SYNOPSIS
+
+    use Bio::Tools::Solubility::Wilkinson;
+
+    my $seq = 'MMAEELLVIKP...'
+
+    my $s = solubility($seq);
+
+=head1 DESCRIPTION
+
+This module implements a simple method for the prediction of protein
+solubility, as described by Wilkinson, D.L. and Harrison [1]. It only
+takes the primary sequence of the protein as a parameter, and returns
+the calculated probability that the protein will be expressed in soluble
+form in E. Coli.
+
+It uses aminoacid composition to compute the two main parameters found
+to have strong correlation with solubility: charge average and turn
+forming residue fraction. For a detailed description of the formula
+used, check the article by Harrison [2] in the L<References> section.
+
+=head1 METHODS
+
+=head2 solubility
+
+Returns the probability of the sequence C<$seq> being soluble. C<$seq>
+should be a correct and validated protein sequence written in one-letter
+aminoacid code with no whitespace or non-residue characters.
+
+    my $probability = solubility($seq);
+
+This function is exported by default.
+
+=head1 Customize your imports
+
+If you'd like to rename the only imported subroutine for some reason,
+you can do:
+
+    use Bio::Tools::Solubility::Wilkinson
+        solubility => { -as => 'solubility_wilkinson' };
+
+    use Some::Other::Solubility::Module 'solubility_foo';
+
+    my $p1 = solubility_wilkinson($seq);
+    my $p2 = solubility_foo($seq);
+
+This (and other goodies) are possible thanks to L<Sub::Exporter>, which
+is worth checking out.
+
+=postlude References
+
+* [1] Wilkinson, D.L. and Harrison, R.G. (1991) BioTechnology 9, 443–448
+L<http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=PubMed&list_uids=1367308&dopt=Abstract>
+
+* [2] R.G. Harrison. 2000. Expression of soluble heterologous proteins via
+fusion with NusA protein. inNovations. 11:4-7.
+L<http://www.biotech.ou.edu/innovations.pdf>
+
+=head1 AUTHOR
+
+  Bruno Vecchi <vecchi.b gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Bruno Vecchi.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
+
+
+
---
+++ t/basic.t
@@ -0,0 +1,65 @@
+use Test::Most;
+use strict;
+use warnings;
+
+use_ok q{ Bio::Tools::Solubility::Wilkinson };
+
+ok solubility('MAELLKKVIKP'), 'subroutine returns a value';
+dies_ok { solubility() }, '... and dies with no arguments';
+
+my $prots = read_sequences('DATA');
+
+cmp_ok
+    abs(solubility($_->{seq}) - $_->{solubility}),
+    '<', 0.02, "Solubility of $_->{name}" for @$prots;
+
+done_testing();
+
+sub read_sequences {
+    my $fh = shift;
+
+    my @entries;
+
+    {
+        local $/ = ">";
+        @entries = <$fh>;
+        shift @entries; # discard first empty record
+    }
+
+    my @sequences;
+
+    foreach my $entry (@entries) {
+
+        $entry =~ s/[\n|>]//g;
+        my ($name, $seq, $solubility) = split /:/, $entry;
+
+        push @sequences, {
+            name => $name, seq => $seq, solubility => $solubility
+        };
+    }
+
+    return \@sequences;
+}
+
+__DATA__
+>gi|242378714|emb|CAQ33504.1| nusA [Escherichia coli BL21(DE3)]:
+MNKEILAVVEAVSNEKALPREKIFEALESALATATKKKYEQEIDVRVQIDRKSGDFDTFRRWLVVDEVTQ
+PTKEITLEAARYEDESLNLGDYVEDQIESVTFDRITTQTAKQVIVQKVREAERAMVVDQFREHEGEIITG
+VVKKVNRDNISLDLGNNAEAVILREDMLPRENFRPGDRVRGVLYSVRPEARGAQLFVTRSKPEMLIELFR
+IEVPEIGEEVIEIKAAARDPGSRAKIAVKTNDKRIDPVGACVGMRGARVQAVSTELGGERIDIVLWDDNP
+AQFVINAMAPADVASIVVDEDKHTMDIAVEAGNLAQAIGRNGQNVRLASQLSGWELNVMTVDDLQAKHQA
+EAHAAIDTFTKYLDIDEDFATVLVEEGFSTLEELAYVPMKELLEIEGLDEPTVEALRERAKNALATIAQA
+QEESLGDNKPADDLLNLEGVDRDLAFKLAARGVCTLEDLAEQGIDDLADIEGLTDEKAGALIMAARNICW
+FGDEA:0.95
+>gi|169757120|gb|ACA79819.1| thioredoxin [Escherichia coli ATCC 8739]:
+MSDKIIHLTDDSFDTDVLKADGAILVDFWAEWCGPCKMIAPILDEIADEYQGKLTVAKLNIDQNPGTAPK
+YGIRGIPTLLLFKNGEVAATKVGALSKGQLKEFLDANLA:0.73
+>gi|345025|emb|CAA00428.1| Bovine growth hormone [synthetic construct]:
+AFPAMSLSGLFANAVLRAQHLHQLAADTFKEFERTYIPEGQRYSIQNTQVAFCFSETMPAPTGKNEAQQK
+SDLELLRISLLLIQSWLGPLQFLSRVFTNSLVFGTSDRVYEKLKDLEEGILALMRELEDGTPRRGQILKQ
+TYDKFDTNMRSDDALLKNYGLLSCFRKDLHKTETYLRVMKCRRFGEASCAF:0.63
+>gi|242378863|emb|CAQ33655.1| bfr [Escherichia coli BL21(DE3)]:
+MKGDTKVINYLNKLLGNELVAINQYFLHARMFKNWGLKRLNDVEYHESIDEMKHADRYIERILFLEGLPN
+LQDLGKLNIGEDVEEMLRSDLALELDGAKNLREAIGYADSVHDYVSRDMMIEILRDEEGHIDWLETELDL
+IQKMGLQNYLQAQIREEG:0.95
+
